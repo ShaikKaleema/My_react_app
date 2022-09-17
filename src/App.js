d8b075415d32a5fcb25761423431cc103fec198c
ShaikKaleema <96244335+ShaikKaleema@users.noreply.github.com>
2022-09-17 10:33:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.css";
+import MyCart from "./MyCart";
+function App() {
+  return (
+    <div>
+      <MyCart />
+    </div>
+  );
+}
+export default App;
